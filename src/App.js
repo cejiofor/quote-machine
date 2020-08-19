95af2ed62e41d7c65aee758564c4210d7c51c442
cejiofor <cejiofor@gmail.com>
2020-08-19 13:48:46 -0500

    Change App from function to class component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ReactFCCtest from 'react-fcctest';
 import {Button, Card, Typography} from '@material-ui/core';
 
-function App() {
-
-  const quote = () => {
-
+class App extends Component {
+  state = {
+    quotes: []
   }
 
-  return (
-    <div className="App">
-      <ReactFCCtest/>
-      <header className="App-header">
-        <Card id="quote-box">
-          <Typography id="text">Quote Placemarker</Typography>  
-          <Typography id="author" color="textSecondary">-Quote Author</Typography>
-          <Button id="new-quote" variant="contained" size="large" color="primary">New Quote</Button>
-          <Typography id="tweet-link">
-          <a
-            className="App-link"
-            id="tweet-quote"
-            href='https://twitter.com/intent/tweet?text=.thisquote'
-            // target="_blank"
-            // rel="noopener noreferrer"
-          >
-            Tweet this Quote!
-          </a>
-          </Typography>
-        </Card>
-        
-      </header>
-      {/* <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script> */}
-    </div>
-  );
+  //Use axios http library to get arry of quotes from server
+  
+  render() {
+    return (
+      <div className="App">
+        <ReactFCCtest/>
+        <header className="App-header">
+          <Card id="quote-box">
+            <Typography id="text">Quote Placemarker</Typography>  
+            <Typography id="author" color="textSecondary">-Quote Author</Typography>
+            <Button id="new-quote" variant="contained" size="large" color="primary">New Quote</Button>
+            <Typography id="tweet-link">
+            <a
+              className="App-link"
+              id="tweet-quote"
+              href='https://twitter.com/intent/tweet?text=.thisquote'
+              // target="_blank"
+              // rel="noopener noreferrer"
+            >
+              Tweet this Quote!
+            </a>
+            </Typography>
+          </Card>
+          
+        </header>
+        {/* <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script> */}
+      </div>
+    )
+  }
 }
 
 export default App;
