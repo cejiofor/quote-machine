8897b6fbe44ffb933c9f363365886cb2d28385df
cejiofor <cejiofor@gmail.com>
2020-09-09 18:31:38 -0500

    Complete basic functionality
    Move API call into App component, using Hooks
    Update Quote display
    Update text target for Twitter intent

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useCallback} from 'react';
 import './App.css';
 import ReactFCCtest from 'react-fcctest';
 import {Button, Card, Typography} from '@material-ui/core';
 import axios from 'axios';
 import Quote from './components/Quote';
-import Quotes from './components/Quotes';
 
-class App extends Component {
-  state = {
-    quotes: []
-  }
+function App() {
+  // state = {
+  //   quote: []
+  // }
 
-  //Use axios http library to get arry of quotes from server
-  componentDidMount(){
-    axios.get('https://type.fit/api/quotes').then(res => this.setState({quotes: res.data}));
-  }
+  // //Use axios http library to get arry of quotes from server
+  // componentDidMount(){
+  //     axios.get('https://type.fit/api/quotes').then(res => {
+  //         console.log(res.data.length)
+  //         let num = Math.floor(Math.random() * res.data.length)
+  //         console.log(num);
+  //         this.setState({quote: res.data[num]})
+  //     });
+  // }
+  
+  const[quote, setQuote] = useState(0);
+  // useEffect(() => {
+  //   axios.get('https://type.fit/api/quotes').then(res => {
+  //         console.log(res.data.length)
+  //         let num = Math.floor(Math.random() * res.data.length)
+  //         console.log(num);
+  //         setQuote(res.data[num]);
+  //     });
+  // });
 
+  const fetchData = useCallback(() => {
+    axios.get('https://type.fit/api/quotes').then(res => {
+      console.log(res.data.length)
+      let num = Math.floor(Math.random() * res.data.length)
+      console.log(num);
+      setQuote(res.data[num]);
+    });
+  });
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        <ReactFCCtest/>
-        <header className="App-header">
-          <Card id="quote-box">
-            {/* <Typography id="text">Quote Placemarker</Typography>  
-            <Typography id="author" color="textSecondary">-Quote Author</Typography> */}
-            {/* <Quote text={this.state.quotes[0]text} author={this.state.quotes[0].author} /> */}
-            <Quote />
-            <Quotes />
-            <Button id="new-quote" variant="contained" size="large" color="primary">New Quote</Button>
-            <Typography id="tweet-link">
-            <a
-              className="App-link"
-              id="tweet-quote"
-              href='https://twitter.com/intent/tweet?text=.thisquote'
-              // target="_blank"
-              // rel="noopener noreferrer"
-            >
-              Tweet this Quote!
-            </a>
-            </Typography>
-          </Card>
-          
-        </header>
-        {/* <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script> */}
-      </div>
-    )
-  }
+  const text = quote.text;
+  const author = quote.author;
+
+  return (
+    <div className="App">
+      {/* <ReactFCCtest/> */}
+      <header className="App-header">
+        <Card id="quote-box">
+          <Quote text={text} author={author}  />
+          <Button id="new-quote" variant="contained" size="large" color="primary" onClick={() => fetchData()}>New Quote</Button>
+          <Typography id="tweet-link">
+          <a
+            className="App-link"
+            id="tweet-quote"
+            href={'https://twitter.com/intent/tweet?text='+text}
+          >
+            Tweet this Quote!
+          </a>
+          </Typography>
+        </Card>
+        
+      </header>
+    </div>
+  )
 }
 
 export default App;
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,37 +3,13 @@ import PropTypes from 'prop-types';
 import {Button, Card, Typography} from '@material-ui/core';
 import axios from 'axios';
 
-export class Quote extends Component {
-    state = {
-        quotes: []
-    }
-    
-    //Use axios http library to get arry of quotes from server
-    componentDidMount(){
-        axios.get('https://type.fit/api/quotes').then(res => this.setState({quotes: res.data[0]}));
-        
-        // const response = axios.get('https://type.fit/api/quotes').then(res => res.data);
-        // this.setState({quote: quotes[0]});
-        
-        // console.log(response)
-    }
-
-    render() {
-    
-        return (
-            <div>
-                <Typography id="text">{this.state.quotes.text}</Typography>  
-                <Typography id="author" color="textSecondary">-{this.state.quotes.author}</Typography>
-                {/* <Typography id="text">{text}</Typography>  
-                <Typography id="author" color="textSecondary">- by {author}</Typography> */}
-            </div>
-        )
-    }
+function Quote({text, author}) {
+    return (
+        <div>
+            <Typography id="text">{text}</Typography>  
+            <Typography id="author" color="textSecondary">-{author}</Typography>
+        </div>
+    )
 }
 
-// //PropTypes
-// Quote.propTypes = {
-//     quote: PropTypes.object.isRequired,
-// }
-
-export default Quote
+export default Quote;
